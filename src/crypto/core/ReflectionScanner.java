f409a91badead1d9a159f24ed1a0b7fa4ddf51b3
Victor Hugo Reinicke <pombewcraft@gmail.com>
2026-06-28 20:42:00 -0300

    Criação da nossa classe que vai usar reflection pra poder manipular as classes que tem o Encrypted

---
+++ src/crypto/core/ReflectionScanner.java
@@ -0,0 +1,14 @@
+package crypto.core;
+
+/**
+ * 
+ * 
+ * Essa classe aqui vai ser pra examinar classes em tempo real indetificando e manipulando os campos protegidos
+ * Pelo que eu vi, a Java Reflection API vai ser usada
+ * 
+ * 
+ * */
+
+public class ReflectionScanner {
+
+}

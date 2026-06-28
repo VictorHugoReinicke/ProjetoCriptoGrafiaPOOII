a8dd46919959227a2600a7c89fc2d469af08689f
Victor Hugo Reinicke <pombewcraft@gmail.com>
2026-06-28 20:51:07 -0300

    Criação do scanner pelos campos criptografados

--- src/crypto/core/ReflectionScanner.java
+++ src/crypto/core/ReflectionScanner.java
@@ -1,5 +1,12 @@
 package crypto.core;
 
+import crypto.annotation.Encrypted;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 /**
  * 
  * 
@@ -11,4 +18,27 @@ package crypto.core;
 
 public class ReflectionScanner {
 
+	
+	/**
+	 *
+	 * Método que vai verificar todas as outras classes que for utilizada procurando pela anotação do @Encrypted
+	 *
+	 * @param classe A classe que vai ser passada
+	 * @return Retorna um array com todos os campos que estão com a anotação
+	 *
+	 * */
+	 public static List<Field> EscanearCamposProtegidos(Class<?> classe) {
+	        List<Field> camposEncrypted = new ArrayList<>();
+	        
+	        Field[] fields = classe.getDeclaredFields(); //pega todos os atributos listados nas classes
+	        
+	        for (Field field : fields) {
+	            if (field.isAnnotationPresent(Encrypted.class)) { //faz um if pra ver se acha a nossa anotação
+	            	camposEncrypted.add(field);
+	            }
+	        }
+	        
+	        return camposEncrypted;
+	    }
+	
 }

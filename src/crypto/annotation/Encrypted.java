da6fcd0f03dc9e6d20292892949df9e1e377cf20
Victor Hugo Reinicke <pombewcraft@gmail.com>
2026-06-28 17:50:52 -0300

    Criação da Interface para poder definir quais atributos vão poder ser criptografados

---
+++ src/crypto/annotation/Encrypted.java
@@ -0,0 +1,15 @@
+package crypto.annotation;
+
+
+/**
+ * 
+ *  Aqui é a parte de notação
+ *  Vai ser utilizado esse Encrypted para poder 
+ *  dizer quais atributos criptografar no projeto
+ * 
+ * 
+**/
+
+public @interface Encrypted {
+
+}

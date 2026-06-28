5868d70d1f43e85150b26ee23a1153ed64cf99a3
Victor Hugo Reinicke <pombewcraft@gmail.com>
2026-06-28 19:30:20 -0300

    Criação da classe que vai utilizar a interface de criptografia

---
+++ src/crypto/core/EncriptadorAES.java
@@ -0,0 +1,25 @@
+package crypto.core;
+
+import crypto.api.CifraStrategy;
+
+public class EncriptadorAES implements CifraStrategy {
+
+	@Override
+	public String encrypt(String textoLimpo, byte[] rawKey) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String decrypt(String textoCripto, byte[] rawKey) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAlgorithmIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
